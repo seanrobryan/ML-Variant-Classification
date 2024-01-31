82d8509c8083e88986e8ad2bbee711057e147458
Sean Ryan <sean-ryan@uiowa.edu>
2024-01-31 14:31:57 -0600

    Removed commented out and unused code

--- src/vcf/vcf_extraction.py
+++ src/vcf/vcf_extraction.py
@@ -13,27 +13,6 @@ import warnings
 chroms_names = ['MT', 'X'] + [str(x) for x in range(1,23)]
 GENOMIC_DESCRIPTION_COL = 'Genomic Description (GRCh37)'
 
-dvd_dir = '/Users/seanryan/Documents/School/Graduate/Variant_Classification_Braun/files_from_MORL_argon/DVD/9_1_1/final_output/'
-morl_dvd_dir = '/home/srryn/hpchome/DVD/versions/9_1_1/final_outputs/'
-
-
-active_dir = morl_dvd_dir
-
-active_dir = os.path.join(active_dir, 'delete_later')
-
-# Open file, this will read in the header
-# dvd_file = os.path.join(active_dir, 'DVDv9_e_20220414.arr.posthoc_annotes_processed_to_ascii.vcf')
-'DVDv9_e_20220414.arr.posthoc_annotes_processed_to_ascii.vcf'
-# dvd_file = os.path.join(active_dir, 'DVDv9_e_20220414.arr.posthoc_annotes_processed_to_ascii.vcf')
-# dvd_gz = os.path.join(active_dir, 'DVDv9_e_20220414.arr.posthoc_annotes.vcf.gz')
-# dvd_bgz = os.path.join(active_dir, 'DVDv9_e_20220414.arr.posthoc_annotes_processed_to_ascii.vcf.bgz')
-
-# dvd_gz = os.path.join(active_dir, 'DVDv9_e_20220414.arr.posthoc_annotes_processed_to_ascii.vcf.gz')
-
-
-dvd_gz = os.path.join(active_dir, 'DVDv9_e_20220414.arr.posthoc_annotes_gunzip_ascii_bgzip.gz')
-dvd_gz_index = os.path.join(active_dir, 'DVDv9_e_20220414.arr.posthoc_annotes_gunzip_ascii_bgzip.gz.tbi')
-
 def separate_chromosomes_from_dvd_vcf(dvd_gz, dvd_gz_index, chromosome_dir='split_vcf_chromosomes_csvs'):
     if not os.path.isdir(chromosome_dir):
         os.makedirs(chromosome_dir)
@@ -42,7 +21,6 @@ def separate_chromosomes_from_dvd_vcf(dvd_gz, dvd_gz_index, chromosome_dir='spli
     chromosomes = {}
     problematic_records = []
     for chromosome in chroms_names:
-        # if chromosome not in ['10']: continue
         file = f"chromosome_{chromosome}_records.csv"
         if os.path.isfile(file):
             print(f"{file} already existings...moving to next chromosome")
@@ -51,7 +29,7 @@ def separate_chromosomes_from_dvd_vcf(dvd_gz, dvd_gz_index, chromosome_dir='spli
                 print("Current chromosome:", chromosome)
                 chromosome_records = reader.fetch(chromosome)
                 cur_vcf_chromosome = VCFChromosome(chromosome)
-                cur_vcf_chromosome.add_records(chromosome_records) # Error occuring here
+                cur_vcf_chromosome.add_records(chromosome_records)
                 cur_vcf_chromosome.update_dataframe()
                 
                 fname = os.path.join(chromosome_dir, f"chromosome_{chromosome}_records")
@@ -60,58 +38,6 @@ def separate_chromosomes_from_dvd_vcf(dvd_gz, dvd_gz_index, chromosome_dir='spli
             except Exception as e:
                 logging.error(traceback.format_exc())
                 print(f"\nSkipping chromosome {chromosome}")
-
-separate_chromsomes_from_dvd_vcf(dvd_gz, dvd_gz_index, 'split_vcf_chromosomes_csvs')
-
-# chromosome_dir = 'split_vcf_chromosomes_csvs'
-# if not os.path.isdir(chromosome_dir):
-#     os.makedirs(chromosome_dir)
-
-# reader = vcfpy.Reader.from_path(dvd_gz, tabix_path=dvd_gz_index)
-# chromosomes = {}
-# problematic_records = []
-# for chromosome in chroms_names:
-#     # if chromosome not in ['10']: continue
-#     file = f"chromosome_{chromosome}_records.csv"
-#     if os.path.isfile(file):
-#         print(f"{file} already existings...moving to next chromosome")
-#     else:
-#         try:
-#             print("Current chromosome:", chromosome)
-#             chromosome_records = reader.fetch(chromosome)
-#             cur_vcf_chromosome = VCFChromosome(chromosome)
-#             cur_vcf_chromosome.add_records(chromosome_records) # Error occuring here
-#             cur_vcf_chromosome.update_dataframe()
-            
-#             fname = os.path.join(chromosome_dir, f"chromosome_{chromosome}_records")
-#             cur_vcf_chromosome.df.to_csv(fname + '.csv')
-#             chromosomes[chromosome] = cur_vcf_chromosome
-#         except Exception as e:
-#             logging.error(traceback.format_exc())
-#             print(f"\nSkipping chromosome {chromosome}")
-
-
-
-for c in chroms_names:
-    utils.extract_tarball(os.path.join(compressed_dir, f"chromosome_{c}_records.tar.gz"))
-
-# # Removing the applied filter line from all the gene files
-# feature_mapped_csv_dir = os.path.join('unzipped', 'featureMappedCsvs')
-# csvs = [os.path.join(feature_mapped_csv_dir, c) for c in os.listdir(feature_mapped_csv_dir)]
-# dfs = []
-# for csv in csvs:
-#     with open(csv, 'r') as file:
-#         lines = file.readlines()
-#         if lines[0].startswith('{'):
-#             lines = lines[1:]
-#     with open(csv, 'w') as file:
-#         file.writelines(lines)
-#     dfs.append(pd.read_csv(csv))
-        
-# dfs = []
-# for csv in csvs:
-#     dfs.append(pd.read_csv(csv))
-
 
 def split_genomic_description(genomic_description):
     chromosome, position, change = genomic_description.split(':')
@@ -124,21 +50,7 @@ def add_genomic_description_cols(df, set_as_index = True):
     df.set_index(GENOMIC_DESCRIPTION_COL, inplace=True)
     return df
 
-# index_cols = ['CHROM', 'REF', 'ALT', 'POS']
-# for df in dfs:
-#     df[index_cols] = pd.DataFrame(df.loc[:, new_index_col].apply(split_genomic_description).tolist(), index=df.index)
-    
-# ddg_df = add_genomic_description_cols(pd.concat(dfs), set_as_index=True)
-# # ddg_df.set_index(new_index_col, inplace=True)
-# ddg_merged_file = 'merged_feature_mapped_ddg_values.csv'
-# ddg_df.to_csv(ddg_merged_file)
- 
-# compressed_chrom_w_ddg_dir = 'compressed_chromosome_w_ddg/'
-# if not os.path.isdir(compressed_chrom_w_ddgo_dir):
-#     os.makedirs(compressed_chrom_w_ddg_dir)
-
 def merge_ddg_dvd_dfs(ddg_file_name, chromosome_dir, save_files_to_dir):
-    # chromosome_ddg_dir = 'chromosomes_w_ddg'
     if not os.path.isdir(save_files_to_dir):
         os.makedirs(save_files_to_dir)
     merged_dfs = {}
@@ -156,16 +68,6 @@ def merge_ddg_dvd_dfs(ddg_file_name, chromosome_dir, save_files_to_dir):
             chrom_w_ddg_file = os.path.join(save_files_to_dir, f"chromosome_{chrom}_w_ddg.csv")
             merged_dfs[chrom].to_csv(chrom_w_ddg_file)
             
-            # # Archived csv
-            # archived_csv = os.path.join(compressed_chrom_w_ddg_dir, os.path.splitext(os.path.basename(chrom_w_ddg_file))[0] + '.tar')
-            # utils.archive_files(chrom_w_ddg_file, archived_csv)
-
-            # # Compress archive
-            # compressed_archive = archived_csv + '.gz'
-            # utils.compress(archived_csv, compressed_archive)
-
-            # os.remove(chrom_w_ddg_file)
-            # os.remove(archived_csv)
 
         except Exception as e:
             print(e)
@@ -178,14 +80,10 @@ os.removedirs(chromosome_ddg_dir)
 
 
 def main(args):
-    # dvd_vcf_gzipped = args[1] # /home/srryn/hpchome/DVD/versions/9_1_1/final_outputs/delete_later/DVDv9_e_20220414.arr.posthoc_annotes_gunzip_ascii_bgzip.gz
-    # dvd_vcf_index_gzipped = args[2] # /home/srryn/hpchome/DVD/versions/9_1_1/final_outputs/delete_later/DVDv9_e_20220414.arr.posthoc_annotes_gunzip_ascii_bgzip.gz.tbi
-    # feature_mapped_csv_dir = args[3] # /unzipped/featureMappedCsvs/
-    # ddg_merged_file = args[4] # merged_feature_mapped_ddg_values.csv
-    dvd_vcf_gzipped = "DVDv9_e_20220414.arr.posthoc_annotes_gunzip_ascii_bgzip.gz"
-    dvd_vcf_index_gzipped = "DVDv9_e_20220414.arr.posthoc_annotes_gunzip_ascii_bgzip.gz.tbi"
-    feature_mapped_csv_dir = "featureMappedCsvs"
-    ddg_merged_file = "merged_feature_mapped_ddg_values.csv"
+    dvd_vcf_gzipped = args[1] # "DVDv9_e_20220414.arr.posthoc_annotes_gunzip_ascii_bgzip.gz"
+    dvd_vcf_index_gzipped = args[2] # "DVDv9_e_20220414.arr.posthoc_annotes_gunzip_ascii_bgzip.gz.tbi"
+    feature_mapped_csv_dir = args[3] # "featureMappedCsvs"
+    ddg_merged_file = args[4] # "merged_feature_mapped_ddg_values.csv"
 
     vcf_chromosome_dir = 'split_vcf_chromosomes_csvs'
     compressed_vcf_chromosome_dir = vcf_chromosome_dir + '_compressed'
